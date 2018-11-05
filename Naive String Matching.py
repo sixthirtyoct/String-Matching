3e82aae54e3be93424b00b3ff227b79c4705bcdb
Ashish Jindal <44592734+sixthirtyoct@users.noreply.github.com>
2018-11-05 10:15:58 +0530

    Update Naive String Matching.py

--- Naive String Matching.py
+++ Naive String Matching.py
@@ -9,3 +9,7 @@ for i in range(len(l1)-len(l2)+1):
     n=i+len(l2)
     if l1[i:n]==l2[:]:
         print("Found with shift",i)
+
+#If n=len(Text), m=len(Pattern)
+#For loop runs n-m+1 times[0...(n-m)] and each time compares 'm' characters in worst case
+Time Complexity =O((n-m+1)m)
